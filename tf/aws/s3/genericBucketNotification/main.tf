766ddd03ef099bb27e1fba7aaf26fc5fd6f67ca3
Scooter Condo <scondo.prof@gmail.com>
2025-02-20 09:58:25 -0500

    dev/finished through the lambda_function argument

--- tf/aws/s3/genericBucketNotification/main.tf
+++ tf/aws/s3/genericBucketNotification/main.tf
@@ -16,9 +16,13 @@ resource "aws_s3_bucket_notification" "bucketNotification" {
   eventbridge = var.bucketNotificationEventbridge
 
   dynamic "lambda_function" {
-    for_each = var.bucketNotification != null ? [var.bucketNotification] : []
+    for_each = var.bucketNotificationLambdaFunction != null ? [var.bucketNotificationLambdaFunction] : []
     content {
-
+      events              = lambda_function.value["events"]
+      filter_prefix       = lambda_function.value["filter_prefix"]
+      filter_suffix       = lambda_function.value["filter_suffix"]
+      id                  = lambda_function.value["id"]
+      lambda_function_arn = lambda_function.value["lambda_function_arn"]
     }
   }
 
--- tf/aws/s3/genericBucketNotification/variables.tf
+++ tf/aws/s3/genericBucketNotification/variables.tf
@@ -5,3 +5,21 @@ variable "awsRegion" {
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_notification#argument-reference
 
+variable "bucketNotificationBucket" {
+  type = string
+}
+
+variable "bucketNotificationEventbridge" {
+  type    = bool
+  default = null
+}
+
+variable "bucketNotificationLambdaFunction" {
+  type = object({
+    events              = list(string)
+    filter_prefix       = optional(string, null)
+    filter_suffix       = optional(string, null)
+    id                  = optional(string, null)
+    lambda_function_arn = string
+  })
+}
