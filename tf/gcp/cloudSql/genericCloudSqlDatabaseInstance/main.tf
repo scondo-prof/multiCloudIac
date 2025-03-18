771c5e79167e9aa06fd8ff2784884965d89906c9
Scooter Condo <scondo.prof@gmail.com>
2025-03-18 15:51:02 -0400

    dev/starting primer for genericCloudSqlDatabaseInstance module

--- tf/gcp/cloudSql/genericCloudSqlDatabaseInstance/main.tf
+++ tf/gcp/cloudSql/genericCloudSqlDatabaseInstance/main.tf
@@ -1,41 +1,46 @@
 provider "google" {
-  project = var.projectId
-  region  = var.region
+  project = var.gcpProjectId
+  region  = var.gcpRegion
 }
 
 resource "google_sql_database_instance" "databaseInstance" {
-  name             = "${var.resourceName}-database-instance"
-  database_version = var.databaseVersion
-  region           = var.region
-  project = var.projectId
-  root_password = var.rootPassword
-  deletion_protection = var.deletionProtection
+  region           = var.gcpRegion
 
-  settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
-    tier = var.tier
-    edition = var.edition
-    user_labels = {
-      "project": var.projectName
-      "deployed-date": var.deployedDate
-      "created-by": var.createdBy
+  dynamic "settings" {
+    for_each = var.databaseInstance != null ? [var.databaseInstance]: []
+    content {
+      
     }
-    activation_policy = var.activationPolicy
-    availability_type = var.availabilityType
-    collation = var.collation
-    disk_autoresize = var.diskAutoresize
-    disk_autoresize_limit = var.diskAutoresizeLimit
-    disk_size = var.diskSize
-    disk_type = var.diskType
-    ip_configuration {
-      ipv4_enabled    = true
+  }
 
-      # Configure the authorized networks to allow access from the specific IP
-      authorized_networks {
-        name  = "${var.resourceName}-authorized-network"
-        value = var.databaseInstanceAuthorizedNetworkIpRange  # Replace with your desired IP address or CIDR block
-      }
+  database_version = var.databaseInstance
+  name = "${var.resourceName}-db"
+  maintenance_version = var.databaseInstance
+  master_instance_name = var.databaseInstance
+  project = var.gcpProjectId
+  
+  dynamic "replica_configuration" {
+    for_each = var.databaseInstance != null ? [var.databaseInstance]: []
+    content {
+      
+    }
+  }
+
+  root_password = var.databaseInstance
+  encryption_key_name = var.databaseInstance
+  deletion_protection = var.databaseInstance
+  
+  dynamic "restore_backup_context" {
+    for_each = var.databaseInstance != null ? [var.databaseInstance]: []
+    content {
+      
+    }
+  }
+
+  dynamic "clone" {
+    for_each = var.databaseInstance != null ? [var.databaseInstance]: []
+    content {
+      
     }
   }
 }
