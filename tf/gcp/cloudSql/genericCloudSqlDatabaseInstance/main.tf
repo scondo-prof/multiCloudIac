c809ccc20ef0f8e47c1fe2ca9dd23f4612cad347
Scooter Condo <scondo.prof@gmail.com>
2025-03-18 16:06:40 -0400

    dev/ finished through the non dynamic arguments of the settings argument

--- tf/gcp/cloudSql/genericCloudSqlDatabaseInstance/main.tf
+++ tf/gcp/cloudSql/genericCloudSqlDatabaseInstance/main.tf
@@ -7,8 +7,116 @@ resource "google_sql_database_instance" "databaseInstance" {
   region           = var.gcpRegion
 
   dynamic "settings" {
-    for_each = var.databaseInstance != null ? [var.databaseInstance]: []
+    for_each = var.databaseInstanceSettings != null ? [var.databaseInstanceSettings]: []
     content {
+      tier = settings.value["tier"]
+      edition = settings.value["edition"]
+
+      user_labels = merge({
+    project       = var.projectName
+    deployed-date = var.deployedDate
+    created-by    = var.createdBy
+    tf-module     = var.tfModule
+  }, var.additionalTags)
+
+      activation_policy = settings.value["activation_policy"]
+      availability_type = settings.value["availability_type"]
+      collation = settings.value["collation"]
+      connector_enforcement = settings.value["connector_enforcement"]
+      deletion_protection_enabled = settings.value["deletion_protection_enabled"]
+      enable_google_ml_integration = settings.value["enable_google_ml_integration"]
+      enable_dataplex_integration = settings.value["enable_dataplex_integration"]
+      disk_autoresize = settings.value["disk_autoresize"]
+      disk_autoresize_limit = settings.value["disk_autoresize_limit"]
+      disk_size = settings.value["disk_size"]
+      disk_type = settings.value["disk_type"]
+      pricing_plan = settings.value["pricing_plan"]
+      time_zone = settings.value["time_zone"]
+      
+      dynamic "advanced_machine_features" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "database_flags" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "active_directory_config" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "data_cache_config" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+      
+      dynamic "deny_maintenance_period" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "sql_server_audit_config" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "backup_configuration" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "ip_configuration" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "location_preference" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "maintenance_window" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "insights_config" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
+      dynamic "password_validation_policy" {
+        for_each = settings.value[""] != null ? [settings.value[""]]: []
+        content {
+          
+        }
+      }
+
       
     }
   }
--- tf/gcp/cloudSql/genericCloudSqlDatabaseInstance/variables.tf
+++ tf/gcp/cloudSql/genericCloudSqlDatabaseInstance/variables.tf
@@ -1,8 +1,8 @@
-variable "projectId"{
+variable "gcpProjectId"{
     type = string
 }
 
-variable "region" {
+variable "gcpRegion" {
   type = string
   default = "us-east1"
 }
@@ -11,96 +11,45 @@ variable "resourceName"{
     type = string
 }
 
-variable "databaseVersion" {
-  type = string
-  validation {
-    condition     = contains(["MYSQL_5_6", "MYSQL_5_7", "MYSQL_8_0", "POSTGRES_9_6", "POSTGRES_10", "POSTGRES_11", "POSTGRES_12", "POSTGRES_13", "POSTGRES_14", "POSTGRES_15", "SQLSERVER_2017_STANDARD", "SQLSERVER_2017_ENTERPRISE", "SQLSERVER_2017_EXPRESS", "SQLSERVER_2017_WEB", "SQLSERVER_2019_STANDARD", "SQLSERVER_2019_ENTERPRISE", "SQLSERVER_2019_EXPRESS", "SQLSERVER_2019_WEB"], var.databaseVersion)
-    error_message = "The only valid options for databaseVersion are MYSQL_5_6, MYSQL_5_7, MYSQL_8_0, POSTGRES_9_6,POSTGRES_10, POSTGRES_11, POSTGRES_12, POSTGRES_13, POSTGRES_14, POSTGRES_15, SQLSERVER_2017_STANDARD, SQLSERVER_2017_ENTERPRISE, SQLSERVER_2017_EXPRESS, SQLSERVER_2017_WEB. SQLSERVER_2019_STANDARD, SQLSERVER_2019_ENTERPRISE, SQLSERVER_2019_EXPRESS, SQLSERVER_2019_WEB."
-  }
-}
-
-variable "rootPassword" {
-  type = string
-}
-
-variable "deletionProtection" {
-  type = bool
-  default = false
-}
-
-variable "tier" {
-  type = string
-}
-
-variable "edition" {
-  type = string
-  validation {
-    condition = contains(["ENTERPRISE", "ENTERPRISE_PLUS"], var.edition)
-    error_message = "The only valid options for edition are ENTERPRISE, ENTERPRISE_PLUS."
-  }
-  default = "ENTERPRISE"
-}
-
-variable "projectName" {
-  type = string
+variable "createdBy" {
+  type    = string
+  default = "scott-condo"
 }
 
 variable "deployedDate" {
   type = string
 }
 
-variable "createdBy" {
+variable "projectName" {
   type = string
-  default = "scott-condo"
 }
 
-variable "activationPolicy" {
+variable "tfModule" {
   type = string
-  validation {
-    condition = contains(["ALWAYS", "NEVER", "ON_DEMAND"], var.activationPolicy)
-    error_message = "The only valid options for activationPolicy are ALWAYS, NEVER or ON_DEMAND."
-  }
-  default = "ALWAYS"
 }
 
-variable "availabilityType" {
-  type = string
-  validation {
-    condition = contains(["REGIONAL", "ZONAL"], var.availabilityType)
-    error_message = "The only valid options for availabilityType are REGIONAL, ZONAL."
-  }
-  default = "ZONAL"
-}
-
-variable "collation" {
-  type = string
+variable "additionalTags" {
+  type    = map(string)
   default = null
 }
 
-variable "diskAutoresize" {
-  type = bool
-  default = true
-}
+#https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#argument-reference
 
-variable "diskAutoresizeLimit" {
-  type = number
-  default = 0
-}
-
-variable "diskSize" {
-  type = number
-  default = 10
-}
-
-variable "diskType" {
-  type = string
-  validation {
-    condition = contains(["PD_SSD","PD_HDD"], var.diskType)
-    error_message = "The only valid options for diskType are REGIONAL, ZONAL."
-  }
-  default = "PD_SSD"
-}
-
-variable "databaseInstanceAuthorizedNetworkIpRange" {
-  type = string
+variable "databaseInstanceSettings" {
+  type = object({
+    tier = string
+    edition = optional(string, null)
+    activation_policy = optional(string, null)
+    availability_type = optional(string, null)
+    collation = optional(string, null)
+    connector_enforcement = optional(string, null)
+    deletion_protection_enabled = optional(bool, null)
+    enable_google_ml_integration = optional(bool, null)
+    enable_dataplex_integration = optional(bool, null)
+    disk_autoresize = optional(bool, null)
+    disk_autoresize_limit = optional(number, null)
+    disk_size = optional(number, null)
+    pricing_plan = optional(string, null)
+    time_zone = optional(string, null)
+  })
 }
