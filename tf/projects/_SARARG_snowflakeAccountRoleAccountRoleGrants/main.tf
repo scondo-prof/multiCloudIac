5032a8766f682a3808316913b56860cabba6ccaa
Scooter Condo <scondo.prof@gmail.com>
2025-04-07 12:09:39 -0400

    dev/repatched SARARG module, running into variable type errors.

--- tf/projects/_SARARG_snowflakeAccountRoleAccountRoleGrants/main.tf
+++ tf/projects/_SARARG_snowflakeAccountRoleAccountRoleGrants/main.tf
@@ -1,34 +1,43 @@
 
 module "accountRole" {
   source                                  = "../../snowflake/principals/genericAccountRole"
+  snowflakeAccountName                    = var.snowflakeAccountName
   snowflakeAuthenticator                  = var.snowflakeAuthenticator
   snowflakeClientIp                       = var.snowflakeClientIp
   snowflakeClientRequestMfaToken          = var.snowflakeClientRequestMfaToken
   snowflakeClientStoreTemporaryCredential = var.snowflakeClientStoreTemporaryCredential
   snowflakeClientTimeout                  = var.snowflakeClientTimeout
+  snowflakeDisableConsoleLogin            = var.snowflakeDisableConsoleLogin
   snowflakeDisableQueryContextCache       = var.snowflakeDisableQueryContextCache
   snowflakeDisableTelemetry               = var.snowflakeDisableTelemetry
+  snowflakeDriverTracing                  = var.snowflakeDriverTracing
   snowflakeExternalBrowserTimeout         = var.snowflakeExternalBrowserTimeout
   snowflakeHost                           = var.snowflakeHost
+  snowflakeIncludeRetryReason             = var.snowflakeIncludeRetryReason
   snowflakeInsecureMode                   = var.snowflakeInsecureMode
   snowflakeJwtClientTimeout               = var.snowflakeJwtClientTimeout
   snowflakeJwtExpireTimeout               = var.snowflakeJwtExpireTimeout
   snowflakeKeepSessionAlive               = var.snowflakeKeepSessionAlive
   snowflakeLoginTimeout                   = var.snowflakeLoginTimeout
+  snowflakeMaxRetryCount                  = var.snowflakeMaxRetryCount
   snowflakeOcspFailOpen                   = var.snowflakeOcspFailOpen
   snowflakeOktaUrl                        = var.snowflakeOktaUrl
+  snowflakeOrganizationName               = var.snowflakeOrganizationName
   snowflakeParams                         = var.snowflakeParams
   snowflakePasscode                       = var.snowflakePasscode
   snowflakePasscodeInPassword             = var.snowflakePasscodeInPassword
   snowflakePassword                       = var.snowflakePassword
   snowflakePort                           = var.snowflakePort
+  snowflakePreviewFeaturesEnabled         = var.snowflakePreviewFeaturesEnabled
   snowflakePrivateKey                     = var.snowflakePrivateKey
   snowflakePrivateKeyPassphrase           = var.snowflakePrivateKeyPassphrase
   snowflakeProfile                        = var.snowflakeProfile
   snowflakeProtocol                       = var.snowflakeProtocol
   snowflakeRequestTimeout                 = var.snowflakeRequestTimeout
   snowflakeRole                           = var.snowflakeRole
+  snowflakeTmpDirectoryPath               = var.snowflakeTmpDirectoryPath
   snowflakeToken                          = var.snowflakeToken
+  snowflakeTokenAccessor                  = var.snowflakeTokenAccessor
   snowflakeUser                           = var.snowflakeUser
   snowflakeValidateDefaultParameters      = var.snowflakeValidateDefaultParameters
   snowflakeWarehouse                      = var.snowflakeWarehouse
@@ -40,34 +49,43 @@ module "accountRole" {
 
 module "grantPrivileges" {
   source                                  = "../../snowflake/principals/genericGrantPrivilegesToAccountRole"
+  snowflakeAccountName                    = var.snowflakeAccountName
   snowflakeAuthenticator                  = var.snowflakeAuthenticator
   snowflakeClientIp                       = var.snowflakeClientIp
   snowflakeClientRequestMfaToken          = var.snowflakeClientRequestMfaToken
   snowflakeClientStoreTemporaryCredential = var.snowflakeClientStoreTemporaryCredential
   snowflakeClientTimeout                  = var.snowflakeClientTimeout
+  snowflakeDisableConsoleLogin            = var.snowflakeDisableConsoleLogin
   snowflakeDisableQueryContextCache       = var.snowflakeDisableQueryContextCache
   snowflakeDisableTelemetry               = var.snowflakeDisableTelemetry
+  snowflakeDriverTracing                  = var.snowflakeDriverTracing
   snowflakeExternalBrowserTimeout         = var.snowflakeExternalBrowserTimeout
   snowflakeHost                           = var.snowflakeHost
+  snowflakeIncludeRetryReason             = var.snowflakeIncludeRetryReason
   snowflakeInsecureMode                   = var.snowflakeInsecureMode
   snowflakeJwtClientTimeout               = var.snowflakeJwtClientTimeout
   snowflakeJwtExpireTimeout               = var.snowflakeJwtExpireTimeout
   snowflakeKeepSessionAlive               = var.snowflakeKeepSessionAlive
   snowflakeLoginTimeout                   = var.snowflakeLoginTimeout
+  snowflakeMaxRetryCount                  = var.snowflakeMaxRetryCount
   snowflakeOcspFailOpen                   = var.snowflakeOcspFailOpen
   snowflakeOktaUrl                        = var.snowflakeOktaUrl
+  snowflakeOrganizationName               = var.snowflakeOrganizationName
   snowflakeParams                         = var.snowflakeParams
   snowflakePasscode                       = var.snowflakePasscode
   snowflakePasscodeInPassword             = var.snowflakePasscodeInPassword
   snowflakePassword                       = var.snowflakePassword
   snowflakePort                           = var.snowflakePort
+  snowflakePreviewFeaturesEnabled         = var.snowflakePreviewFeaturesEnabled
   snowflakePrivateKey                     = var.snowflakePrivateKey
   snowflakePrivateKeyPassphrase           = var.snowflakePrivateKeyPassphrase
   snowflakeProfile                        = var.snowflakeProfile
   snowflakeProtocol                       = var.snowflakeProtocol
   snowflakeRequestTimeout                 = var.snowflakeRequestTimeout
   snowflakeRole                           = var.snowflakeRole
+  snowflakeTmpDirectoryPath               = var.snowflakeTmpDirectoryPath
   snowflakeToken                          = var.snowflakeToken
+  snowflakeTokenAccessor                  = var.snowflakeTokenAccessor
   snowflakeUser                           = var.snowflakeUser
   snowflakeValidateDefaultParameters      = var.snowflakeValidateDefaultParameters
   snowflakeWarehouse                      = var.snowflakeWarehouse
@@ -78,3 +96,5 @@ module "grantPrivileges" {
   grantPrivilegesAlwaysApplyTrigger       = var.SARARG_GrantPrivilegesAlwaysApplyTrigger
   grantPrivilegesOnAccount                = var.SARARG_GrantPrivilegesOnAccount
 }
+
+#---
