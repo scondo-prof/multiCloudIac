ac8feb34ee3ec3cdda3a0d9135988d8bb80b1b22
Scooter Condo <scondo.prof@gmail.com>
2025-04-11 11:16:31 -0400

    dev/genericDistribution primer established

---
+++ tf/aws/cloudfront/genericDistribution/variables.tf
@@ -0,0 +1,33 @@
+variable "awsRegion" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "resourceName" {
+  type = string
+}
+
+variable "projectName" {
+  type = string
+}
+
+variable "createdBy" {
+  type    = string
+  default = "scott-condo"
+}
+
+variable "deployedDate" {
+  type = string
+}
+
+variable "tfModule" {
+  type = string
+}
+
+variable "additionalTags" {
+  type    = map(string)
+  default = {}
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_distribution#argument-reference
+
