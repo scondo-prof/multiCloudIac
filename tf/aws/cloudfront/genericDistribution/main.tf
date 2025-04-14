5293f0165be0ea3a0e13ad21f0653476ed2181d3
Scooter Condo <scondo.prof@gmail.com>
2025-04-14 10:51:09 -0400

    dev/completed arguments through custom_error_response argument

--- tf/aws/cloudfront/genericDistribution/main.tf
+++ tf/aws/cloudfront/genericDistribution/main.tf
@@ -11,3 +11,60 @@ provider "aws" {
   region = var.awsRegion
 }
 
+resource "aws_cloudfront_distribution" "cdn" {
+  aliases =  var.cdnAliases
+  comment = var.cdnComment
+  continuous_deployment_policy_id = var.cdnContinuousDeploymentPolicyId
+  
+  dynamic "custom_error_response" {
+    for_each = var.cdnCustomErrorResponse != null ? var.cdnCustomErrorResponse: []
+    content {
+      error_caching_min_ttl = custom_error_response.value["error_caching_min_ttl"]
+      error_code = custom_error_response.value["error_code"]
+      response_code = custom_error_response.value["response_code"]
+      response_page_path = custom_error_response.value["response_page_path"]
+    }
+  }
+
+  dynamic "default_cache_behavior" {
+    
+  }
+
+  default_root_object = var.cdn
+  enabled = var.cdn
+  is_ipv6_enabled = var.cdn
+  http_version = var.cdn
+  
+  dynamic "logging_config" {
+    
+  }
+
+  dynamic "ordered_cache_behavior" {
+    
+  }
+
+  origin {
+    
+  }
+
+  dynamic "origin_group" {
+    
+  }
+
+  price_class = var.cdn
+  
+  restrictions {
+    
+  }
+
+  staging = var.cdn
+  tags = var.cdn
+  
+  viewer_certificate {
+    
+  }
+
+  web_acl_id = var.cdn
+  retain_on_delete = var.cdn
+  wait_for_deployment = var.cdn
+}
--- tf/aws/cloudfront/genericDistribution/variables.tf
+++ tf/aws/cloudfront/genericDistribution/variables.tf
@@ -31,3 +31,27 @@ variable "additionalTags" {
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_distribution#argument-reference
 
+variable "cdnAliases" {
+  type = list(string)
+  default = null
+}
+
+variable "cdnComment" {
+  type = string
+  default = null
+}
+
+variable "cdnContinuousDeploymentPolicyId" {
+  type = string
+  default = null
+}
+
+variable "cdnCustomErrorResponse" {
+  type = list(object({
+    error_caching_min_ttl = optional(number, null)
+    error_code = number
+    response_code = optional(number, null)
+    response_page_path = optional(string, null)
+  }))
+  default = null
+}
