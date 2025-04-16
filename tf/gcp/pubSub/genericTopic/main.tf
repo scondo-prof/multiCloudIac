64e9d9b93486f7ddf587d2b17cb09b64e1db1b61
Scooter Condo <scondo.prof@gmail.com>
2025-04-16 15:00:19 -0400

    dev/finished through labels argument

--- tf/gcp/pubSub/genericTopic/main.tf
+++ tf/gcp/pubSub/genericTopic/main.tf
@@ -14,4 +14,36 @@ provider "google" {
 
 resource "google_pubsub_topic" "topic" {
   name = var.topicName
+  kms_key_name = var.topicKmsKeyName
+  labels = merge({
+    project       = var.projectName
+    deployed-date = var.deployedDate
+    created-by    = var.createdBy
+    tf-module     = var.tfModule
+  }, var.additionalTags)
+  
+  dynamic "message_storage_policy" {
+    for_each = var.topic != null ? [var.topic] : []
+    content {
+      
+    }
+  }
+
+  dynamic "schema_settings" {
+    for_each = var.topic != null ? [var.topic] : []
+    content {
+      
+    }
+  }
+
+  message_retention_duration = var.topic
+
+  dynamic "ingestion_data_source_settings" {
+    for_each = var.topic != null ? [var.topic] : []
+    content {
+      
+    }
+  }
+
+  project = var.gcpProjectId
 }
--- tf/gcp/pubSub/genericTopic/variables.tf
+++ tf/gcp/pubSub/genericTopic/variables.tf
@@ -11,4 +11,35 @@ variable "resourceName" {
   type = string
 }
 
+variable "projectName" {
+  type = string
+}
+
+variable "deployedDate" {
+  type = string
+}
+
+variable "createdBy" {
+  type = string
+  default = "scott-condo"
+}
+
+variable "tfModule" {
+  type = string
+}
+
+variable "additionalTags" {
+  type = map(string)
+  default = {}
+}
+
 #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_topic#argument-reference
+
+variable "topicName" {
+  type = string
+}
+
+variable "topicKmsKeyName" {
+  type = string
+  default = null
+}
