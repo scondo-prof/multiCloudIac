f870d7ebdd8ee9a18f69d92f27bd424ad214c50a
Scooter Condo <scondo.prof@gmail.com>
2025-04-21 16:16:41 -0400

    dev/finished through query_strings_config sub argument in parameters_in_cache_key_and_forwarded_to_origin argument

--- tf/aws/cloudfront/genericCachePolicy/variables.tf
+++ tf/aws/cloudfront/genericCachePolicy/variables.tf
@@ -27,3 +27,27 @@ variable "cachePolicyComment" {
   type = string
   default = null
 }
+
+variable "cachePolicyParametersInCacheKeyAndForwardToOrigin" {
+  type = object({ #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_cache_policy#parameters-in-cache-key-and-forwarded-to-origin
+    cookie_behavior = string
+
+    cookies = optional(object({ #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_cache_policy#items
+      items = list(string)
+    }), null)
+
+    header_behavior = string
+
+    headers = optional(object({ #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_cache_policy#items
+      items = list(string)
+    }), null)
+
+    query_string_behavior = string
+
+    query_strings = optional(object({ #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_cache_policy#items
+      items = list(string)
+    }), null)
+
+    
+  })
+}
