2aea508ef8637c701d313c8e4e7b27e7e08761bf
Scooter Condo <scondo.prof@gmail.com>
2025-04-21 16:04:08 -0400

    dev/finished through comment argument

--- tf/aws/cloudfront/genericCachePolicy/main.tf
+++ tf/aws/cloudfront/genericCachePolicy/main.tf
@@ -13,20 +13,39 @@ provider "aws" {
 
 resource "aws_cloudfront_cache_policy" "cachePolicy" {
   name = "${var.resourceName}-cache-policy"
-  min_ttl = var.cachePolicy
-  max_ttl = var.cachePolicy
-  default_ttl = var.cachePolicy
-  comment = var.cachePolicy
+  min_ttl = var.cachePolicyMinTtl
+  max_ttl = var.cachePolicyMaxTtl
+  default_ttl = var.cachePolicyDefaultTtl
+  comment = var.cachePolicyComment
   
   parameters_in_cache_key_and_forwarded_to_origin {
     cookies_config {
+      cookie_behavior = var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""]
       
+      dynamic "cookies" {
+        for_each = var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""] != null ? [var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""]]: []
+        content {
+          
+        }
+      }
     }
     headers_config {
-      
+      header_behavior = var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""]
+      dynamic "headers" {
+        for_each = var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""] != null ? [var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""]]: []
+        content {
+          
+        }
+      }
     }
     query_strings_config {
-      
+      query_string_behavior = var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""]
+      dynamic "query_strings" {
+        for_each = var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""] != null ? [var.cachePolicyParametersInCacheKeyAndForwardToOrigin[""]]: []
+        content {
+          
+        }
+      }
     }
     enable_accept_encoding_brotli = var.cachePolicyParametersInCacheKeyAndForwardToOrigin["enable_accept_encoding_brotli"]
     enable_accept_encoding_gzip = var.cachePolicyParametersInCacheKeyAndForwardToOrigin["enable_accept_encoding_brotli"]
--- tf/aws/cloudfront/genericCachePolicy/variables.tf
+++ tf/aws/cloudfront/genericCachePolicy/variables.tf
@@ -3,4 +3,27 @@ variable "awsRegion" {
   default = "us-east-1"
 }
 
+variable "resourceName" {
+  type = string
+}
+
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudfront_cache_policy#argument-reference
+
+variable "cachePolicyMinTtl" {
+  type = number
+}
+
+variable "cachePolicyMaxTtl" {
+  type = number
+  default = null
+}
+
+variable "cachePolicyDefaultTtl" {
+  type = number
+  default = null
+}
+
+variable "cachePolicyComment" {
+  type = string
+  default = null
+}
