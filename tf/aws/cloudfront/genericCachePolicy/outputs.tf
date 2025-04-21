25be50a99df87045528d2dbd390fa9965822820c
Scooter Condo <scondo.prof@gmail.com>
2025-04-21 16:32:27 -0400

    dev/finished genericCachePolicy module

--- tf/aws/cloudfront/genericCachePolicy/outputs.tf
+++ tf/aws/cloudfront/genericCachePolicy/outputs.tf
@@ -1,47 +1,11 @@
-output "cdnId" {
-  value = aws_cloudfront_distribution.cdn.id
+output "cachePolicyArn" {
+  value = aws_cloudfront_cache_policy.cachePolicy.arn
 }
 
-output "cdnArn" {
-  value = aws_cloudfront_distribution.cdn.arn
+output "cachePolicyEtag" {
+  value = aws_cloudfront_cache_policy.cachePolicy.etag
 }
 
-output "cdnCallerReference" {
-  value = aws_cloudfront_distribution.cdn.caller_reference
-}
-
-output "cdnStatus" {
-  value = aws_cloudfront_distribution.cdn.status
-}
-
-output "cdnTagsAll" {
-  value = aws_cloudfront_distribution.cdn.tags_all
-}
-
-output "cdnTrustedKeyGroups" {
-  value = aws_cloudfront_distribution.cdn.trusted_key_groups
-}
-
-output "cdnTrustedSigners" {
-  value = aws_cloudfront_distribution.cdn.trusted_signers
-}
-
-output "cdnDomainName" {
-  value = aws_cloudfront_distribution.cdn.domain_name
-}
-
-output "cdnLastModifiedTime" {
-  value = aws_cloudfront_distribution.cdn.last_modified_time
-}
-
-output "cdnInProgressValidationBatches" {
-  value = aws_cloudfront_distribution.cdn.in_progress_validation_batches
-}
-
-output "cdnETag" {
-  value = aws_cloudfront_distribution.cdn.etag
-}
-
-output "cdnHostedZoneId" {
-  value = aws_cloudfront_distribution.cdn.hosted_zone_id
+output "cachePolicyId" {
+  value = aws_cloudfront_cache_policy.cachePolicy.id
 }
