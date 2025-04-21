6d00765ca0c293915455b32d156c9f46da8f645f
Scooter Condo <scondo.prof@gmail.com>
2025-04-21 15:11:35 -0400

    dev/finished through filter argument

--- tf/gcp/pubSub/genericSubscription/main.tf
+++ tf/gcp/pubSub/genericSubscription/main.tf
@@ -13,7 +13,7 @@ provider "google" {
 }
 
 resource "google_pubsub_subscription" "subscription" {
-  name         = "${var.resourceName}-subscription"
+  name  = "${var.resourceName}-subscription"
   topic = var.subscriptionTopic
   labels = merge({
     project       = var.projectName
@@ -23,32 +23,32 @@ resource "google_pubsub_subscription" "subscription" {
   }, var.additionalTags)
 
   dynamic "bigquery_config" {
-    for_each = var.subscriptionBigqueryConfig != null ? [var.subscriptionBigqueryConfig]: []
+    for_each = var.subscriptionBigqueryConfig != null ? [var.subscriptionBigqueryConfig] : []
     content {
-      table = bigquery_config.value["table"]
-      use_table_schema = bigquery_config.value["use_table_schema"]
-      use_topic_schema = bigquery_config.value["use_topic_schema"]
-      write_metadata = bigquery_config.value["write_metadata"]
-      drop_unknown_fields = bigquery_config.value["drop_unknown_fields"]
+      table                 = bigquery_config.value["table"]
+      use_table_schema      = bigquery_config.value["use_table_schema"]
+      use_topic_schema      = bigquery_config.value["use_topic_schema"]
+      write_metadata        = bigquery_config.value["write_metadata"]
+      drop_unknown_fields   = bigquery_config.value["drop_unknown_fields"]
       service_account_email = bigquery_config.value["service_account_email"]
     }
   }
 
   dynamic "cloud_storage_config" {
-    for_each = var.subscriptionCloudStorageConfig != null ? [var.subscriptionCloudStorageConfig]: []
+    for_each = var.subscriptionCloudStorageConfig != null ? [var.subscriptionCloudStorageConfig] : []
     content {
-      bucket = cloud_storage_config.value["bucket"]
-      filename_prefix = cloud_storage_config.value["filename_prefix"]
-      filename_suffix = cloud_storage_config.value["filename_suffix"]
+      bucket                   = cloud_storage_config.value["bucket"]
+      filename_prefix          = cloud_storage_config.value["filename_prefix"]
+      filename_suffix          = cloud_storage_config.value["filename_suffix"]
       filename_datetime_format = cloud_storage_config.value["filename_datetime_format"]
-      max_duration = cloud_storage_config.value["max_duration"]
-      max_bytes = cloud_storage_config.value["max_bytes"]
-      max_messages = cloud_storage_config.value["max_messages"]
-      
+      max_duration             = cloud_storage_config.value["max_duration"]
+      max_bytes                = cloud_storage_config.value["max_bytes"]
+      max_messages             = cloud_storage_config.value["max_messages"]
+
       dynamic "avro_config" {
-        for_each = cloud_storage_config.value["avro_config"] != null ? [cloud_storage_config.value["avro_config"]]: []
+        for_each = cloud_storage_config.value["avro_config"] != null ? [cloud_storage_config.value["avro_config"]] : []
         content {
-          write_metadata = avro_config.value["write_metadata"]
+          write_metadata   = avro_config.value["write_metadata"]
           use_topic_schema = avro_config.value["use_topic_schema"]
         }
       }
@@ -58,40 +58,56 @@ resource "google_pubsub_subscription" "subscription" {
   }
 
   dynamic "push_config" {
-    for_each = var.subscription != null ? [var.subscription]: []
+    for_each = var.subscriptionPushConfig != null ? [var.subscriptionPushConfig] : []
     content {
-      
+      dynamic "oidc_token" {
+        for_each = push_config.value["oidc_token"] != null ? [push_config.value["oidc_token"]] : []
+        content {
+          service_account_email = oidc_token.value["service_account_email"]
+          audience              = oidc_token.value["audience"]
+        }
+      }
+
+      push_endpoint = push_config.value["push_endpoint"]
+      attributes    = push_config.value["attributes"]
+
+      dynamic "no_wrapper" {
+        for_each = push_config.value["no_wrapper"] != null ? [push_config.value["no_wrapper"]] : []
+        content {
+          write_metadata = no_wrapper.value["write_metadata"]
+        }
+      }
     }
   }
 
-  ack_deadline_seconds = var.subscription
-  message_retention_duration = var.subscription
-  retain_acked_messages = var.subscription
-  
+  ack_deadline_seconds       = var.subscriptionAckDeadlineSeconds
+  message_retention_duration = var.subscriptionMessageRetentionDuration
+  retain_acked_messages      = var.subscriptionRetainAckedMessages
+
   dynamic "expiration_policy" {
-    for_each = var.subscription != null ? [var.subscription]: []
+    for_each = var.subscriptionExpirationPolicy != null ? [var.subscriptionExpirationPolicy] : []
     content {
-      
+      ttl = expiration_policy.value["ttl"]
     }
   }
 
-  filter = var.subscription
+  filter = var.subscriptionFilter
 
   dynamic "dead_letter_policy" {
-    for_each = var.subscription != null ? [var.subscription]: []
+    for_each = var.subscription != null ? [var.subscription] : []
     content {
-      
+
     }
   }
 
   dynamic "retry_policy" {
-    for_each = var.subscription != null ? [var.subscription]: []
+    for_each = var.subscription != null ? [var.subscription] : []
     content {
-      
+
     }
   }
 
-  enable_message_ordering = var.subscription
+  enable_message_ordering      = var.subscription
   enable_exactly_once_delivery = var.subscription
-  project = var.gcpProjectId
+  project                      = var.gcpProjectId
 }
--- tf/gcp/pubSub/genericSubscription/variables.tf
+++ tf/gcp/pubSub/genericSubscription/variables.tf
@@ -40,12 +40,12 @@ variable "subscriptionTopic" {
 }
 
 variable "subscriptionBigqueryConfig" {
-  type = object({#https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_bigquery_config
-    table = string
-    use_table_schema = optional(bool, null)
-    use_topic_schema = optional(bool, null)
-    write_metadata = optional(bool, null)
-    drop_unknown_fields = optional(bool, null)
+  type = object({ #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_bigquery_config
+    table                 = string
+    use_table_schema      = optional(bool, null)
+    use_topic_schema      = optional(bool, null)
+    write_metadata        = optional(bool, null)
+    drop_unknown_fields   = optional(bool, null)
     service_account_email = optional(string, null)
   })
   default = null
@@ -53,16 +53,16 @@ variable "subscriptionBigqueryConfig" {
 
 variable "subscriptionCloudStorageConfig" {
   type = object({ #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_cloud_storage_config
-    bucket = string
-    filename_prefix = optional(string, null)
-    filename_suffix = optional(string, null)
+    bucket                   = string
+    filename_prefix          = optional(string, null)
+    filename_suffix          = optional(string, null)
     filename_datetime_format = optional(string, null)
-    max_duration = optional(string, null)
-    max_bytes = optional(string, null)
-    max_messages = optional(number, null)
-    
+    max_duration             = optional(string, null)
+    max_bytes                = optional(string, null)
+    max_messages             = optional(number, null)
+
     avro_config = optional(object({ #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_cloud_storage_config_avro_config
-      write_metadata = optional(bool, null)
+      write_metadata   = optional(bool, null)
       use_topic_schema = optional(bool, null)
     }), null)
 
@@ -71,3 +71,50 @@ variable "subscriptionCloudStorageConfig" {
   default = null
 }
 
+variable "subscriptionPushConfig" {
+  type = object({                  #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_push_config
+    oidc_token = optional(object({ #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_push_config_oidc_token
+      service_account_email = string
+      audience              = optional(string, null)
+    }), null)
+
+    push_endpoint = string
+    attributes    = optional(string, null)
+
+    no_wrapper = optional(object({ #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_push_config_no_wrapper
+      write_metadata = bool
+    }), null)
+  })
+  default = null
+}
+
+variable "subscriptionAckDeadlineSeconds" {
+  type = number
+  validation {
+    condition     = var.subscriptionAckDeadlineSeconds == null || can(var.subscriptionAckDeadlineSeconds >= 0 && var.subscriptionAckDeadlineSeconds <= 600)
+    error_message = "var.subscriptionAckDeadlineSeconds must be Greater than or Equal to 0 AND Less Than or Equal to 600"
+  }
+  default = null
+}
+
+variable "subscriptionMessageRetentionDuration" {
+  type    = string
+  default = null
+}
+
+variable "subscriptionRetainAckedMessages" {
+  type    = bool
+  default = null
+}
+
+variable "subscriptionExpirationPolicy" {
+  type = object({ #https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/pubsub_subscription#nested_expiration_policy
+    ttl = string
+  })
+  default = null
+}
+
+variable "subscriptionFilter" {
+  type    = string
+  default = null
+}
